fd52f4dd66d08b81ce3d7e4f1584331b2ece78de
unknown <kevinvluu@gmail.com>
2019-04-15 13:53:09 -0700

    as defined

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,4 +1,34 @@
 
-public class Schedule {
-
+public class Schedule 
+{
+	
+	public Schedule()
+	{
+		
+	}
+	
+	public Job insert(int time)
+	{
+	
+		return new Job();
+	}
+	
+	public Job get(int index)
+	{
+		
+		return new Job();
+	}
+	public int finish()
+	{
+		
+		return 0;
+	}
+	
+	public class Job
+	{
+		private Job()
+		{
+			
+		}
+	}
 }
